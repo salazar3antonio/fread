2a774ac79ac20256c008a4262f9f098a474d3c9e
Antonio Salazar III <salazar3antonio@gmail.com>
2018-02-21 20:02:54 -0600

    User Only Sees Their Articles
    I have setup a query for the db reference to only display articles saved by the currently signed in user.
    
    I still need to fix the db from refreshing when user changes accounts. Right now the app must be destroyed and restarted for new user saved articles to show.

--- app/src/main/java/com/freadapp/fread/MainActivity.java
+++ app/src/main/java/com/freadapp/fread/MainActivity.java
@@ -2,34 +2,27 @@ package com.freadapp.fread;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.freadapp.fread.data.model.Article;
-import com.freadapp.fread.data.model.Articles;
 import com.freadapp.fread.helpers.Constants;
 import com.freadapp.fread.signin_classes.SignInActivity;
 import com.freadapp.fread.view_holder.ArticleViewHolder;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import com.google.firebase.database.Query;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     private DatabaseReference mArticlesDBref;
     private FirebaseRecyclerAdapter mFirebaseAdapter;
     private RecyclerView mRecyclerView;
+    private Query mQueryByUserArticles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,27 +53,44 @@ public class MainActivity extends AppCompatActivity {
         FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
         mArticlesDBref = firebaseDatabase.getReferenceFromUrl(Constants.ARTICLES_REFERENCE);
 
-        mArticlesDBref.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+        //check to see if user is logged in.
+        if (mUser == null) {
+            Toast.makeText(getApplicationContext(), "No user logged in.", Toast.LENGTH_SHORT).show();
+        } else {
+            //query by articles of the user that is logged in
+            mQueryByUserArticles = mArticlesDBref.orderByChild("uid").equalTo(mUser.getUid());
+            mArticlesDBref.addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                    Article article = dataSnapshot.getValue(Article.class);
+                    Log.i(TAG, "Article Titles: " + article.getTitle());
+                    Log.i(TAG, "User UID: " + article.getUid());
+                    setFirebaseAdapter();
 
-                final ArrayList<Article> articles = new ArrayList<Article>();
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    articles.add(snapshot.getValue(Article.class));
-                    Log.i(TAG, "Article Titles: " + snapshot.getValue(Article.class).getTitle());
                 }
 
-                Log.i(TAG, "Number of Articles: " + articles.size());
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
 
-            }
+                }
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
 
-            }
-        });
+                }
 
-        setFirebaseAdapter();
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+            setFirebaseAdapter();
+        }
 
         mSignInButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -94,10 +105,21 @@ public class MainActivity extends AppCompatActivity {
     private void setFirebaseAdapter() {
 
         mFirebaseAdapter = new FirebaseRecyclerAdapter<Article, ArticleViewHolder>(Article.class, R.layout.article_list_item,
-                ArticleViewHolder.class, mArticlesDBref) {
+                ArticleViewHolder.class, mQueryByUserArticles) {
 
             @Override
             protected void populateViewHolder(ArticleViewHolder viewHolder, Article model, int position) {
+
+                final Article article = model;
+
+                viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //launch a new detailed article activity passing the article at the clicked position through an intent
+
+                        Toast.makeText(getApplicationContext(), article.getTitle() , Toast.LENGTH_SHORT).show();
+                    }
+                });
                 viewHolder.bindToArticle(model);
             }
         };
@@ -105,6 +127,8 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mRecyclerView.setAdapter(mFirebaseAdapter);
+
+
 
     }
 
